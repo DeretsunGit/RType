2d25a813867adea0c42dc68397a524b2e8cfe082
edmond_j <edmond_j@epitech.eu>
2013-11-04 19:20:03 +0000

    Adding ASocket ctor and dtor

---
+++ Proj/RType/RType/ASocket.cpp
@@ -0,0 +1,22 @@
+//
+// ASocket.cpp for RType in /home/edmond_j//rendu/B5/Rtype/RType/Proj/RType/RType
+//
+// Made by julien edmond
+// Login   <edmond_j@epitech.net>
+//
+// Started on  Mon Nov  4 19:16:18 2013 julien edmond
+// Last update Mon Nov  4 19:17:28 2013 julien edmond
+//
+
+#include	"SocketPool.hpp"
+#include	"Socket.h"
+
+ASocket::ASocket()
+{
+  SocketPool::getInstance().watchSocket(this);
+}
+
+ASocket::~ASocket()
+{
+  SocketPool::getInstance().releaseSocket(this);
+}
