ff34801c727d439ebc8380457621e69d5505356c
edmond_j <edmond_j@epitech.eu>
2013-11-07 17:17:46 +0100

    Forgot the file

---
+++ Proj/RType/RType/IReadableSocket.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include  "Socket.h"
+#include  "Packet.hpp"
+
+class IReadableSocket: public ISocket
+{
+public:
+  virtual ~IReadableSocket() {}
+
+  virtual unsigned int	recv(char* buff, unsigned int size) = 0;
+  virtual void		send(const char* buff, unsigned int size) = 0;
+  virtual void		send(const Packet& p) = 0;
+  virtual unsigned int	readable() const = 0;
+};
