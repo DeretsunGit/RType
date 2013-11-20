cd5563f332fe9654205245206bb3fd82d203e3ce
miramo_m <miramo_m@epitech.eu>
2013-11-20 15:06:50 +0100

    je vais en anglais !

--- Proj/RType/RType/Wave.cpp
+++ Proj/RType/RType/Wave.cpp
@@ -1,7 +1,8 @@
 #include "ESprites.h"
 #include "Wave.h"
 
-Wave::Wave(std::list<Bullet*> &bulletPool)
+Wave::Wave(std::list<float> timePopWave)
+: _timePopWave(timePopWave)
 {
 	this->_pos._posX = 1650;
 	this->_pos._posY = 450;
@@ -11,38 +12,11 @@ Wave::~Wave()
 {
 }
 
-std::list<eSprites>	Wave::fillSpriteEnemy(eSprites firstSprite, int nb)
-{
-	int cpt = 0;
-	std::list<eSprites> sprites;
-
-	while (cpt < nb)
-	{
-		sprites.push_back(static_cast<eSprites>(firstSprite + cpt));
-		++cpt;
-	}
-	return (sprites);
-}
-
-void	Wave::init()
-{
-	int					nb = 10;
-	std::list<eSprites>	sprites;
-
-	sprites = this->fillSpriteEnemy(ENEMY_RED_1, 8);
-	while (nb < 0)
-	{
-		this->_ennemy.push_back(new Ennemy(nb, this->_pos, sprites)); //TODO: revoir l'attribution des id avec collegue 
-		--nb;
-	}
-}
-
-std::list<Ennemy*>	Wave::action()
+void	Wave::action(std::list<Ennemy*> ennemies, std::list<Bullet*> bullets, float gameTime)
 {
 	this->popEnnemy();
 	this->moveEnnemy();
 	this->moveBullet();
-	return (this->_ennemy);
 }
 
 void	Wave::popEnnemy()
--- Proj/RType/RType/Wave.h
+++ Proj/RType/RType/Wave.h
@@ -7,19 +7,17 @@
 
 class Wave
 {
-	std::list<Ennemy*>	_ennemy;
 	t_coord				_pos;
-	Clock				_timeFire;
-	Clock				_timePop;
+	Clock				_timeFireEnnemy;
+	Clock				_timePopEnnemy;
 	char				_color;
+	std::list<float>	_timePopWave;
 public:
-	Wave(std::list<Bullet*> &);
+	Wave(std::list<float>);
 	~Wave();
 
-	void				init();
-	std::list<Ennemy*>	action();
+	void				action(std::list<Ennemy*>, std::list<Bullet*>, float);
 	void				popEnnemy();
 	void				moveEnnemy();
 	void				moveBullet();
-	std::list<eSprites>	fillSpriteEnemy(eSprites, int);
 };
